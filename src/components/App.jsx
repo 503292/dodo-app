e42f0bdee53705bd23a0260ec6f85baa7c0d57ca
Kalysh <503292@ukr.net>
2019-12-10 17:02:25 +0200

    CONNECT _ Header to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Header from './Header/Header';
+import Header from './Header/Header';
 import Main from './Main/Main';
 // import Footer from './Footer/';
 
 const App = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Main />
       {/* <Footer /> */}
     </>
