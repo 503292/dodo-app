79e6bf8e7771a6d20706024be3cdcef26bf0e29f
Kalysh <503292@ukr.net>
2019-12-12 18:25:40 +0200

    DROP _ material-ui / some styling components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 
 // material-ui
-// import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 
 import DollarRate from './DollarRate/DollarRate';
 import Weather from './Weather/Weather';
@@ -22,21 +16,9 @@ class Header extends Component {
     return (
       <>
         <div className={css.wrapHeader}>
-          <AppBar position="static">
-            <Toolbar>
-              <Avatar alt="Remy Sharp" src="../../../../" />
-              <Typography
-                variant="h6"
-                // className={classes.title}
-              >
-                Dodo
-              </Typography>
-              <Button color="inherit">Login</Button>
-            </Toolbar>
-          </AppBar>
-
           <Logo />
           <Weather />
+
           <DollarRate />
           <BtnLogin />
         </div>
