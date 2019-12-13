27fd5d3033839ad2f927e9238778785e3c34ff84
kalysh_maxym <503292@ukr.net>
2019-12-14 01:42:16 +0300

    REFACT _ DollarRate component

--- src/components/Header/DollarRate/DollarRate.jsx
+++ src/components/Header/DollarRate/DollarRate.jsx
@@ -5,7 +5,7 @@ import css from './DollarRate.module.css';
 const DollarRate = () => (
   <>
     <div className={css.wrapRate}>
-      <p className={css.dollar}>1 ${/* € */}</p>
+      <p className={css.dollar}>${/* € */}</p>
       <p className={css.gryvnya}>24.5 грн</p>
     </div>
   </>
