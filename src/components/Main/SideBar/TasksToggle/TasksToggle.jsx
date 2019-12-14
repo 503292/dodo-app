0e5bdef8a95f0c04c7b9669f776630ccf0f98a4a
kalysh_maxym <503292@ukr.net>
2019-12-14 19:25:00 +0200

    REFACT _ Task menu

--- src/components/Main/SideBar/TasksToggle/TasksToggle.jsx
+++ src/components/Main/SideBar/TasksToggle/TasksToggle.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import css from './TasksToggle.module.css';
 
 const TasksToggle = () => (
   <>
     <div className={css.wrapTasksToggle}>
-      <div className={css.titleTasksToggle}>
-        ◷ Завдання / Всі
-        <div>
-          <span className={css.red} role="img" aria-label="dot">
-            ⚫
-          </span>
-          <span className={css.yellow} role="img" aria-label="dot">
-            ⚫
-          </span>
-          <span className={css.green} role="img" aria-label="dot">
-            ⚫
-          </span>
-        </div>
+      <div className={css.titleTasksToggle}>◷ Завдання / Всі</div>
+      <div className={css.wrapTasksLevels}>
+        <FontAwesomeIcon className={css.red} icon={faCircle} />
+        <FontAwesomeIcon className={css.yellow} icon={faCircle} />
+        <FontAwesomeIcon className={css.green} icon={faCircle} />
       </div>
 
-      <div className={css.groupBtnTasks}>
+      {/* <div className={css.groupBtnTasks}>
         <button className={css.hardTasks} type="button">
           HARD
         </button>
@@ -30,7 +24,7 @@ const TasksToggle = () => (
         <button className={css.lowTasks} type="button">
           LOW
         </button>
-      </div>
+      </div> */}
     </div>
   </>
 );
