154cea9dc36b02fb0aca2faee22917241977b851
kalysh_maxym <503292@ukr.net>
2020-02-22 07:07:58 +0200

    CHANGE _ logo

--- src/components/Header/BtnLogin/BtnLogin.jsx
+++ src/components/Header/BtnLogin/BtnLogin.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faAddressBook } from '@fortawesome/free-regular-svg-icons';
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
+
 import css from './BtnLogin.module.css';
 
 const BtnIn = () => (
   <div className={css.wrapBtn}>
-    <button className={css.login} type="button">
+    <FontAwesomeIcon className={css.goOut} icon={faPowerOff} />
+    {/* <button className={css.login} type="button">
       Login
-    </button>
+    </button> */}
   </div>
 );
 
--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import logo from '../../../assets/image/logo.png';
+import { ReactComponent as LogoMap } from '../../../assets/image/mapUa.svg';
+
 import css from './Logo.module.css';
 
 const Logo = () => (
   <div className={css.wrapLogo}>
-    <img className={css.logoImg} src={logo} width="40" height="40" alt="logo" />
+    <LogoMap className={css.logoMap} />
+    {/* <img className={css.logoImg} src={logo} width="40" height="40" alt="logo" /> */}
     {/* <p className={css.titleSite}>DoDo</p> */}
   </div>
 );
