8dadcc54bdb8af5c39f80eb95eca0a9b07d6e18d
kalysh_maxym <503292@ukr.net>
2020-03-27 22:58:39 +0200

    ADD _ weather icons / PREPARE _ switcher for icons

--- src/components/HeaderNav/WeatherNav/WeatherNav.jsx
+++ src/components/HeaderNav/WeatherNav/WeatherNav.jsx
@@ -1,56 +1,107 @@
 import React, { Component } from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloudSun } from '@fortawesome/free-solid-svg-icons';
-// import { fetchDarkSkyWeather } from '../../../services/api';
+import icons from './weatherIcons';
+import parseWeatherData from './ParseWorlWeather';
 import { fetchWorldWeather } from '../../../services/api';
 
 import css from './WeatherNav.module.css';
+
+// wind S SW SSW SSE SE ESE E NE NNE N NNW  NW WNW WSW ENE
 
 class WeatherNav extends Component {
   state = {
     weather: '',
   };
 
+  // getIcon = (descUk) => {
+  //   switch(descUk) {
+  //     case 'Ясно':
+  //     case 'Сонячно':
+  //       ...
+  //       break;
+
+  //     case 'Невелика хмарність':
+
+  //       ...
+  //       break;
+  //     case 'Суцільна хмарність':
+  //     case 'Хмарно':
+  //       ...
+  //       break;
+  //     case 'Невеликий сніг':
+  //     case 'Місцями невеликий сніг':
+  //     case 'Помірний або сильний снігопад':
+  //     case 'Низова хуртовина':
+  //     case 'Помірний сніг':
+  //     case 'Заметіль':
+  //     case 'Сильний сніг':
+  //       ...
+  //       break;
+  //     case 'Мряка':
+  //     case 'Туман':
+  //     case 'Туман з памороззю':
+
+  //       ...
+  //       break;
+  //     case 'Місцями дощ':
+  //     case 'Час від часу помірний дощ':
+  //     case 'Невелика злива':
+  //     case 'Невеликий дощ':
+  //     case 'Місцями невеликий дощ':
+  //     case 'Помірна або сильна злива':
+  //     case 'Помірний дощ':
+  //     case 'Сильний дощ':
+  //     case 'Дуже сильна злива':
+
+  //       ...
+  //       break;
+  //     case 'Місцями невеликий дощ інколи з грозою':
+  //     case 'Невеликий дощ зі снігом':
+  //       ...
+  //       break;
+  //     case 'Місцями грози':
+  //       ...
+  //       break;
+
+  //     default:
+  //       ...
+  //       break;
+  //   }
+
+  // };
+
   componentDidMount() {
     fetchWorldWeather('Kiev')
       .then(data => {
-        // localStorage.setItem('localWeather', JSON.stringify(data));
         console.log(data, 'data');
-        // this.setState({
-        //   weather: data,
-        // });
+        const parseData = parseWeatherData(data);
+        localStorage.setItem('localWeather', JSON.stringify(parseData));
+        console.log(parseData, 'parseData');
+        this.setState({
+          weather: parseData,
+        });
       })
       .catch(error => {
         console.log(error, 'err wheather darkSky');
       });
-    // fetchDarkSkyWeather('50.45,30.51')
-    //   .then(data => {
-    //     localStorage.setItem('localWeather', JSON.stringify(data));
-    //     this.setState({
-    //       weather: data,
-    //     });
-    //   })
-    //   .catch(error => {
-    //     console.log(error, 'err wheather darkSky');
-    //   });
   }
 
   render() {
     const { weather } = this.state;
-
+    // weather.currentWeather.descUk
     console.log(weather, 'weather');
-    if (weather) {
-      console.log(weather.currently.temperature, 'weather');
-    }
+    // console.log(icons.cloud_moon, 'cloud-moon-rain.svg');
 
     return (
       <>
         {weather && (
           <>
             <div className={css.wrapWeather}>
-              <FontAwesomeIcon className={css.cloudSun} icon={faCloudSun} />
-              <p className={css.degree}>{weather.currently.temperature}&deg;</p>
+              <div className={css.wrapIcon}>
+                <img className={css.icon} src={icons.thermometer} alt="img" />
+              </div>
+
+              <p className={css.degree}>{weather.currentWeather.tempC}&deg;</p>
             </div>
             <div className={css.timezone}>{weather.timezone}</div>
           </>
---
+++ src/components/HeaderNav/WeatherNav/weatherIcons.js
@@ -0,0 +1,25 @@
+import cloudMoonRain from '../../../assets/image/weather/cloud-moon-rain.svg';
+import cloudMoon from '../../../assets/image/weather/cloud-moon.svg';
+import cloudShowersHeavy from '../../../assets/image/weather/cloud-showers-heavy.svg';
+import cloudSunRain from '../../../assets/image/weather/cloud-sun-rain.svg';
+import cloudSun from '../../../assets/image/weather/cloud-sun.svg';
+import cloud from '../../../assets/image/weather/cloud.svg';
+import moon from '../../../assets/image/weather/moon.svg';
+import smog from '../../../assets/image/weather/smog.svg';
+import snow from '../../../assets/image/weather/snowflake.svg';
+import sun from '../../../assets/image/weather/sun.svg';
+import thermometer from '../../../assets/image/weather/thermometer.svg';
+
+export default {
+  cloud_moon_rain: cloudMoonRain,
+  cloud_moon: cloudMoon,
+  cloud_showers_heavy: cloudShowersHeavy,
+  cloud_sun_rain: cloudSunRain,
+  cloud_sun: cloudSun,
+  cloud,
+  moon,
+  smog,
+  snow,
+  sun,
+  thermometer,
+};
