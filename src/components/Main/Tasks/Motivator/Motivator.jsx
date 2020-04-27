fba1bfa12d795e3b0ead8043169e3c8bb5cd89e9
kalysh_maxym <503292@ukr.net>
2020-04-27 17:44:21 +0300

    STYLED _ refresh motivator

--- src/components/Main/Tasks/Motivator/Motivator.jsx
+++ src/components/Main/Tasks/Motivator/Motivator.jsx
@@ -4,7 +4,8 @@ import css from './Motivator.module.css';
 import motivations from './motivations';
 
 import { ReactComponent as Exclamation } from '../../../../assets/image/exclamation.svg';
-// import { ReactComponent as Quotes } from '../../../../assets/image/quotes.svg';
+import { ReactComponent as Refresh } from '../../../../assets/image/refresh.svg';
+import { ReactComponent as Refresh1 } from '../../../../assets/image/refresh-1.svg';
 
 class Motivator extends Component {
   state = {
@@ -16,11 +17,21 @@ class Motivator extends Component {
     this.setState({ motivationNumber });
   }
 
+  // componentDidUpdate(prevProps, prevState) {
+  //   const { motivationNumber } = this.state;
+  //   if (prevState.motivationNumber !== motivationNumber) {
+  //     this.setState({ motivationNumber });
+  //   }
+  // }
+
   randomNumber = arr => Math.floor(Math.random() * arr.length);
+
+  // refreshMotivation = e => {
+  //   e.preventDefault;
+  // };
 
   render() {
     const { motivationNumber } = this.state;
-    // console.log(motivations.length, 'mot length');
     let oneMotive = '';
     let oneAuthor = '';
 
@@ -28,13 +39,16 @@ class Motivator extends Component {
       oneMotive = motivations[motivationNumber].motivation;
       oneAuthor = motivations[motivationNumber].author;
     }
-    // console.log(oneMotive, 'oneMotive');
-    // console.log(oneAuthor, 'oneAuthor');
+
     return (
       <>
         <div className={css.container}>
           <Exclamation className={css.exclamation} />
-          {/* <Quotes className={css.quotes} /> */}
+          <button type="button" className={css.btnRefresh}>
+            {/* <Refresh className={css.refresh} /> */}
+            <Refresh1 className={css.refresh} />
+          </button>
+
           <p className={css.text}>{oneMotive}</p>
           {oneAuthor ? (
             <span className={css.author}>{oneAuthor}</span>
