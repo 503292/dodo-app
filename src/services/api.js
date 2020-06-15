8e144fee250f699393a420764df4914609443d97
kalysh_maxym <503292@ukr.net>
2020-06-15 22:22:28 +0300

    FIX _ infoSite / ADD _ new key for weather

--- src/services/api.js
+++ src/services/api.js
@@ -24,7 +24,8 @@ export const fetchCurrencyNBU = () => {
 const baseWorldWeather =
   'http://api.worldweatheronline.com/premium/v1/weather.ashx';
 
-const apiKey = 'b71a24e3a604422a9a263408200405';
+// const apiKey = 'b71a24e3a604422a9a263408200405';  // end 05.07.2020 ?
+const apiKey = '482a809a7ca8447893c190815201506'; // end 14.08.2020 500pcs
 const settings2 =
   '&format=json&num_of_days=7&extra=localObsTime,isDayTime&fx=yes&cc=yes&mca=no&tp=1&showlocaltime=yes&lang=uk';
 
