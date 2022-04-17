05168c613a70f61ea11d6bb2e8a89b50e293f89e
503292 <503292@ukr.net>
2022-04-18 01:18:15 +0300

    add weatherFolk #381

--- src/components/commons/NavbarTop/NavbarTop.jsx
+++ src/components/commons/NavbarTop/NavbarTop.jsx
@@ -42,7 +42,7 @@ const NavbarTop = () => {
       </li>
       <li className={css.routeHeader}>
         <NavLink to="/currency" exact activeStyle={ativeStyle}>
-          <CurrencyNav />в
+          <CurrencyNav />
         </NavLink>
       </li>
 
--- src/components/commons/WeatherFolk/WeatherFolk.jsx
+++ src/components/commons/WeatherFolk/WeatherFolk.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import dayjs from 'dayjs';
+import dayOfYear from 'dayjs/plugin/dayOfYear';
+
+import weatherFolk from './weatherFolkData';
 
 import css from './WeatherFolk.module.scss';
 
 const WeatherFolk = () => {
   // const today = new Date().
+  // console.log(weatherFolk, 'weatherFolk');
+  dayjs.extend(dayOfYear);
 
-  return <div className={css.wrapWeatherFolk}>WeatherFolk</div>;
+  const getDayOfYear = () => {
+    return dayjs(new Date()).dayOfYear();
+  };
+
+  console.log(getDayOfYear(), 'getDayOfYear');
+
+  function getDayFolk(num, arr) {
+    // TODO fix for 29march year (365 to 366 days)
+    // const folk = arr[num - 1];
+    const folk = arr[num];
+    return folk;
+  }
+
+  return (
+    <div className={css.wrapWeatherFolk}>
+      {getDayFolk(getDayOfYear(), weatherFolk)}
+    </div>
+  );
 };
 
 export default WeatherFolk;
