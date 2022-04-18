4cd3a7dd17a1378217dc2cd0b778c207ae3eb573
503292 <503292@ukr.net>
2022-04-18 15:28:12 +0300

    change bitcoin icon

--- src/components/Header/CurrencyNav/switchCurrency.js
+++ src/components/Header/CurrencyNav/switchCurrency.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { ReactComponent as Dollar } from '../../../assets/image/currency/dollar.svg';
 import { ReactComponent as Euro } from '../../../assets/image/currency/euro.svg';
 import { ReactComponent as Ruble } from '../../../assets/image/currency/ruble.svg';
-import { ReactComponent as Btc } from '../../../assets/image/currency/btc.svg';
+import { ReactComponent as Bitcoin } from '../../../assets/image/currency/bitcoin.svg';
 
 export default function switchCurrency(currency) {
   switch (currency) {
@@ -14,7 +14,7 @@ export default function switchCurrency(currency) {
     case 'RUB':
       return <Ruble />;
     case 'BTC':
-      return <Btc />;
+      return <Bitcoin />;
 
     default:
       return '';
