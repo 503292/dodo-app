07cd8068868f5183fbb2564a3ff6de43bb9e7c17
503292 <503292@ukr.net>
2022-04-18 16:58:32 +0300

    folkWeather - clean code / some fix

--- src/components/commons/WeatherFolk/WeatherFolk.jsx
+++ src/components/commons/WeatherFolk/WeatherFolk.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
 import dayOfYear from 'dayjs/plugin/dayOfYear';
@@ -7,29 +7,26 @@ import weatherFolk from './weatherFolkData';
 
 import css from './WeatherFolk.module.scss';
 
+// helpers
+function getDayOfYear(indexDay) {
+  return dayjs(new Date()).dayOfYear() + indexDay;
+}
+
+function getDayFolk(num, arr) {
+  // TODO fix for 29march year (365 to 366 days)
+  // const folk = arr[num - 1];
+  const folk = arr[num];
+  return folk;
+}
+
 const WeatherFolk = ({ indexDay }) => {
-  // const today = new Date().
-  // console.log(weatherFolk, 'weatherFolk');
   dayjs.extend(dayOfYear);
 
-  const getDayOfYear = () => {
-    return dayjs(new Date()).dayOfYear() + indexDay;
-  };
+  const folk = useMemo(() => {
+    return getDayFolk(getDayOfYear(indexDay), weatherFolk);
+  }, [indexDay]);
 
-  // console.log(getDayOfYear(), 'getDayOfYear');
-
-  function getDayFolk(num, arr) {
-    // TODO fix for 29march year (365 to 366 days)
-    // const folk = arr[num - 1];
-    const folk = arr[num];
-    return folk;
-  }
-
-  return (
-    <div className={css.wrapWeatherFolk}>
-      {getDayFolk(getDayOfYear(), weatherFolk)}
-    </div>
-  );
+  return <p className={css.wrapWeatherFolk}>{folk}</p>;
 };
 
 WeatherFolk.propTypes = {
