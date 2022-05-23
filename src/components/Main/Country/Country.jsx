addafc2308dd111d49faa0a1a6d06023b6ab60f1
503292 <503292@ukr.net>
2022-05-23 10:24:22 +0300

    Country - refact mapUa styles

--- src/components/Main/Country/Country.jsx
+++ src/components/Main/Country/Country.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 
-import VideoPlayer from '../../commons/VideoPlayer/VideoPlayer';
+// import VideoPlayer from '../../commons/VideoPlayer/VideoPlayer';
 import BoxWithBorder from '../../commons/BoxWithBorder/BoxWithBorder';
 
-import ukraineMapUrl from '../../../assets/image/UA/map_ukraine.jpg';
-import patelnyaMapUrl from '../../../assets/image/UA/map_patelnya.jpg';
+// import ukraineMapUrl from '../../../assets/image/UA/map_ukraine.jpg';
+// import patelnyaMapUrl from '../../../assets/image/UA/map_patelnya.jpg';
 import { ReactComponent as MapUa } from '../../../assets/image/logo/mapUa.svg';
 
 import css from './Country.module.scss';
 
 const Country = () => {
   // TODO helper with key videos
-  const videoUrl = 'https://www.youtube.com/embed/ZATdVxeksjU';
+  // const videoUrl = 'https://www.youtube.com/embed/ZATdVxeksjU';
 
   return (
     <div className={css.wrapCountry}>
       <div className={css.wrapFielset}>
-        <BoxWithBorder title="Карта України).svg">
+        <BoxWithBorder title="Карта України).">
           <div className={`${css.wrapMap} ${css.blueYellow}`}>
             <MapUa />
           </div>
         </BoxWithBorder>
-        <BoxWithBorder title="UA, Україна, Львів-Червоноград 80103">
+        {/* <BoxWithBorder title="UA, Україна, Львів-Червоноград 80103">
           <div className={css.wrapMap}>
             <img src={ukraineMapUrl} alt="map" />
           </div>
@@ -30,13 +30,13 @@ const Country = () => {
           <div className={css.wrapMap}>
             <img src={patelnyaMapUrl} alt="map" />
           </div>
-        </BoxWithBorder>
+        </BoxWithBorder> */}
         <h3 className={css.title}>UA, Україна, Червоноград 80103</h3>
       </div>
 
-      <div className={css.wrapVideo}>
+      {/* <div className={css.wrapVideo}>
         <VideoPlayer url={videoUrl} />
-      </div>
+      </div> */}
       <div className={css.wrapAudio}></div>
     </div>
   );
