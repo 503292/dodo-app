133832a1744adb73d5d451ead65c6f7a9af35798
maksym.kalysh <maksym.kalysh@devabit.com>
2022-11-02 15:55:02 +0200

    change wheather key

--- src/services/api.js
+++ src/services/api.js
@@ -30,7 +30,7 @@ export const fetchCurrencyNBU = () => {
 const baseWorldWeather =
   'http://api.worldweatheronline.com/premium/v1/weather.ashx';
 
-const apiKey = 'ebd4b68c6c8c4bfab56153653222609'; // Trial ending on 25 Oct, 2022
+const apiKey = 'f9b8e2b303c74170bc8135335220211'; // Trial ending on 01 Dec, 2022
 const settings2 =
   '&format=json&num_of_days=7&extra=localObsTime,isDayTime&fx=yes&cc=yes&mca=no&tp=1&showlocaltime=yes&lang=uk';
 export const fetchWorldWeather = query => {
