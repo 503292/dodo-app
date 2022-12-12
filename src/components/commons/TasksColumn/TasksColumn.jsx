769448a7a2300b2c28e47335e28fb316885a8bd1
maksym.kalysh <maksym.kalysh@devabit.com>
2022-12-12 15:24:40 +0200

    refact TasksColumn component

--- src/components/commons/TasksColumn/TasksColumn.jsx
+++ src/components/commons/TasksColumn/TasksColumn.jsx
@@ -7,59 +7,54 @@ import Motivator from '../Motivator/Motivator';
 
 import css from './TasksColumn.module.scss';
 
-class TasksColumn extends Component {
-  state = {};
+const TasksColumn = ({
+  tasksDraw,
+  column,
+  modalAddTasksOpen,
+  updateCompleted,
+  updateTask,
+  deleteTask,
+}) => {
+  return (
+    <div className={css.wrapColumn}>
+      <Motivator />
 
-  render() {
-    const {
-      tasksDraw,
-      column,
-      modalAddTasksOpen,
-      updateCompleted,
-      updateTask,
-      deleteTask,
-    } = this.props;
+      {tasksDraw.length <= 0 && (
+        <div>
+          <p className={css.start}>
+            Подумай що ти можеш зробити преємного для себе і близьких у цьому
+            році. Шукай плюси свого життя. Подумай скільки всього у тебе є. Все
+            що можна уявити реально).
+          </p>
+        </div>
+      )}
+      <Droppable droppableId={column.id}>
+        {provided => {
+          return (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {tasksDraw?.map((task, index) => (
+                <Task
+                  key={task.id}
+                  task={task}
+                  index={index}
+                  updateCompleted={updateCompleted}
+                  modalAddTasksOpen={modalAddTasksOpen}
+                  updateTask={updateTask}
+                  deleteTask={deleteTask}
+                />
+              ))}
+              {provided.placeholder}
+            </div>
+          );
+        }}
 
-    return (
-      <div className={css.wrapColumn}>
-        <Motivator />
+        {/* TODO */}
+        {/* <span style={{ height: '2rem' }} /> */}
+      </Droppable>
+    </div>
+  );
+};
 
-        {tasksDraw.length <= 0 && (
-          <div>
-            <p className={css.start}>
-              Подумай що ти можеш зробити преємного для себе і близьких у цьому
-              році. Шукай плюси свого життя. Подумай скільки всього у тебе є.
-              Все що можна уявити реально).
-            </p>
-          </div>
-        )}
-        <Droppable droppableId={column.id}>
-          {provided => {
-            return (
-              <div ref={provided.innerRef} {...provided.droppableProps}>
-                {tasksDraw.map((task, index) => (
-                  <Task
-                    key={task.id}
-                    task={task}
-                    index={index}
-                    updateCompleted={updateCompleted}
-                    modalAddTasksOpen={modalAddTasksOpen}
-                    updateTask={updateTask}
-                    deleteTask={deleteTask}
-                  />
-                ))}
-                {provided.placeholder}
-              </div>
-            );
-          }}
-
-          {/* TODO */}
-          {/* <span style={{ height: '2rem' }} /> */}
-        </Droppable>
-      </div>
-    );
-  }
-}
 TasksColumn.propTypes = {
   tasksDraw: PropTypes.arrayOf(PropTypes.shape({}).isRequired).isRequired,
   column: PropTypes.shape(PropTypes.arrayOf().isRequired).isRequired,
