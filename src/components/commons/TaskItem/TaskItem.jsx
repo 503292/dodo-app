005cb5560a32a2f110a62776b8df72882f6bfdc2
maksym.kalysh <maksym.kalysh@devabit.com>
2022-12-12 15:18:15 +0200

    refact TaskItem comonent

--- src/components/commons/TaskItem/TaskItem.jsx
+++ src/components/commons/TaskItem/TaskItem.jsx
@@ -9,10 +9,18 @@ import { getColor } from '../../../utils/Priority';
 
 import css from './TaskItem.module.scss';
 
-class TaskItem extends Component {
-  state = {};
+const TaskItem = ({
+  task,
+  index,
+  modalAddTasksOpen,
+  updateCompleted,
+  updateTask,
+  deleteTask,
+}) => {
+  const { text } = task;
+  const splitText = text.split(/\r?\n/);
 
-  formatEndTime = endTime => {
+  const formatEndTime = endTime => {
     let hour = new Date(endTime).getHours();
     if (hour < 10) hour = '0'.concat(hour);
     let minutes = new Date(endTime).getMinutes();
@@ -20,106 +28,89 @@ class TaskItem extends Component {
 
     return `${hour}:${minutes}`;
   };
-
-  render() {
-    const {
-      task,
-      index,
-      modalAddTasksOpen,
-      updateCompleted,
-      updateTask,
-      deleteTask,
-    } = this.props;
-
-    const { text } = task;
-    const splitText = text.split(/\r?\n/);
-
-    return (
-      <>
-        <Draggable
-          className={css.dragContainer}
-          draggableId={task.id}
-          index={index}
-        >
-          {(provided, snapshot) => (
-            // eslint-disable-next-line
-            <div
-              {...provided.draggableProps}
-              {...provided.dragHandleProps}
-              ref={provided.innerRef}
-              isdragging={snapshot.isDragging.toString()}
-              onClick={this.onClick}
-              className={css.dragContainer}
-            >
-              <div
-                className={`${css.task} ${css[`${getColor(task.priority)}`]}`}
-              >
-                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-                <label className={css.wrapText}>
-                  <input
-                    className={css.completedCheckbox}
-                    type="checkbox"
-                    checked={task.completed}
-                    onChange={() => updateCompleted(task.id)}
-                  />
-                  <div
-                    className={
-                      task.completed === true
-                        ? `${css.text} ${css.lineThrough}`
-                        : css.text
-                    }
-                  >
-                    {splitText.map((el, ind) => {
-                      // eslint-disable-next-line react/no-array-index-key
-                      return <p key={ind}>{el}</p>;
-                    })}
-                  </div>
-                </label>
-
-                <div className={css.actions}>
-                  {!task.completed ? (
-                    <button
-                      type="button"
-                      onClick={() => {
-                        modalAddTasksOpen();
-                        updateTask(task);
-                      }}
-                      value={task.id}
-                      className={css.wrapIcon}
-                    >
-                      <Pen className={css.penIcon} />
-                    </button>
-                  ) : (
-                    <button
-                      type="button"
-                      onClick={() => {
-                        deleteTask(task.id);
-                      }}
-                      value={task.id}
-                      className={css.wrapIcon}
-                    >
-                      <Delete className={css.deleteIcon} />
-                    </button>
-                  )}
-
-                  <p
-                    className={
-                      task.completed === true
-                        ? `${css.completedCheckbox} ${css.endTime}`
-                        : css.endTime
-                    }
-                  >
-                    {this.formatEndTime(task.endTime)}
-                  </p>
+  return (
+    <>
+      <Draggable
+        className={css.dragContainer}
+        draggableId={task.id}
+        index={index}
+      >
+        {(provided, snapshot) => (
+          // eslint-disable-next-line
+          <div
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            isdragging={snapshot.isDragging.toString()}
+            // onClick={this.onClick}
+            className={css.dragContainer}
+          >
+            <div className={`${css.task} ${css[`${getColor(task.priority)}`]}`}>
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label className={css.wrapText}>
+                <input
+                  className={css.completedCheckbox}
+                  type="checkbox"
+                  checked={task.completed}
+                  onChange={() => updateCompleted(task.id)}
+                />
+                <div
+                  className={
+                    task.completed === true
+                      ? `${css.text} ${css.lineThrough}`
+                      : css.text
+                  }
+                >
+                  {splitText.map((el, ind) => {
+                    // eslint-disable-next-line react/no-array-index-key
+                    return <p key={ind}>{el}</p>;
+                  })}
                 </div>
+              </label>
+
+              <div className={css.actions}>
+                {!task.completed ? (
+                  <button
+                    type="button"
+                    onClick={() => {
+                      modalAddTasksOpen();
+                      updateTask(task);
+                    }}
+                    value={task.id}
+                    className={css.wrapIcon}
+                  >
+                    <Pen className={css.penIcon} />
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={() => {
+                      deleteTask(task.id);
+                    }}
+                    value={task.id}
+                    className={css.wrapIcon}
+                  >
+                    <Delete className={css.deleteIcon} />
+                  </button>
+                )}
+
+                <p
+                  className={
+                    task.completed === true
+                      ? `${css.completedCheckbox} ${css.endTime}`
+                      : css.endTime
+                  }
+                >
+                  {formatEndTime(task.endTime)}
+                </p>
               </div>
             </div>
-          )}
-        </Draggable>
-      </>
-    );
-  }
-}
+          </div>
+        )}
+      </Draggable>
+    </>
+  );
+};
 
 TaskItem.propTypes = {
   task: PropTypes.shape(PropTypes.arrayOf().isRequired).isRequired,
