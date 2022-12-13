8af3a80aeda762ec7c87e2c73783da8733222171
maksym.kalysh <maksym.kalysh@devabit.com>
2022-12-13 23:51:38 +0200

    refact Tasks component #2

--- src/components/Main/Tasks2/Tasks.jsx
+++ src/components/Main/Tasks2/Tasks.jsx
@@ -24,25 +24,84 @@ const Tasks = ({
     },
   });
   const [columnOrder, setColumnOrder] = useState(['column-1']);
-  const [editTask, setEitTask] = useState(null);
+  const [editTask, setEditTask] = useState(null);
+
+  const viewTasks = data => {
+    // eslint-disable-next-line func-names
+    const filtered = data.filter(function(el) {
+      return el != null;
+    });
+    // TODO filter by sec/m/h/days
+    // console.log(filtered, 'filtered');
+
+    const arrIds = filtered?.map(el => el.id);
+    // const { columns } = this.state;
+    const arr = columns['column-1'].tasksIds;
+    setTasks(data);
+    setColumns({
+      'column-1': {
+        id: 'column-1',
+        title: 'Сьогодні',
+        tasksIds: arrIds.length === arr.length ? arr : arrIds,
+      },
+    });
+    setEditTask(null);
+    setColumnOrder(['column-1']);
+  };
+
+  useEffect(() => {
+    const localTasksArr = JSON.parse(localStorage.getItem('localTasks'));
+    // const { addTaskToRedux } = this.props;
+    // const { data } = this.props;
+
+    if (localTasksArr) {
+      // eslint-disable-next-line func-names
+      const filtered = localTasksArr.filter(function(el) {
+        return el != null;
+      });
+
+      filtered.map(el => {
+        const ids = data?.map(d => d.id);
+
+        if (!ids.includes(el?.id)) {
+          addTaskToRedux(el);
+        }
+        return '';
+      });
+      viewTasks(localTasksArr);
+    }
+  }, []);
+
+  useEffect(() => {
+    // const { data } = this.props;
+    // if (prevProps.data !== data) {
+    viewTasks(data);
+    // }
+
+    // save in localStorage new arrTask & update sort after drop
+    // if (prevState !== this.state) {
+    // const { columns } = this.state;
+    const tasksIdArr = columns['column-1'].tasksIds;
+    const localTasksArr = JSON.parse(localStorage.getItem('localTasks'));
+
+    const newTasks = [];
+    tasksIdArr.forEach(el => {
+      const oneTask = localTasksArr.find(t => t.id === el);
+      newTasks.push(oneTask);
+    });
+
+    localStorage.setItem('localTasks', JSON.stringify(newTasks));
+  }, [data]);
 
   const updateCompleted = async id => {
-    // const { updateIsCompletedTaskToRedux } = this.props;
     const localTasksArr = JSON.parse(localStorage.getItem('localTasks'));
     // TODO
-    await this.setTasks(state => ({
-      tasks: state.tasks.map(task =>
+    const tmp = await setTasks(
+      tasks.map(task =>
         task.id === id ? { ...task, completed: !task.completed } : task,
       ),
-    }));
+    );
 
-    await this.setState(state => ({
-      tasks: state.tasks.map(task =>
-        task.id === id ? { ...task, completed: !task.completed } : task,
-      ),
-    }));
-
-    const { tasks } = this.state;
     const taskTmp = tasks.find(el => el.id === id);
     updateIsCompletedTaskToRedux(taskTmp);
 
@@ -53,11 +112,118 @@ const Tasks = ({
     localStorage.setItem('localTasks', JSON.stringify(localTasksArr));
   };
 
+  const updateTask = task => {
+    setEditTask(task);
+  };
+
+  const deleteTask = id => {
+    // const { deleteTaskFromRedux } = this.props;
+    deleteTaskFromRedux(id);
+  };
+
+  const onDragUpdate = update => {
+    const { destination } = update;
+    // const { tasks } = this.state;
+    const opacity = destination
+      ? destination.index / Object.keys(tasks).length
+      : 0;
+
+    document.body.style.backgroundColor = `rgba(170,225,250, ${opacity})`;
+
+    document.body.style.transition = 'background-color 0.9s ease';
+  };
+
+  const onDragEnd = async result => {
+    document.body.style.color = 'inherit';
+    // const { updateAllTasksToRedux } = this.props;
+    // const { columns } = this.state;
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) {
+      return;
+    }
+
+    const startColumn = columns[source.droppableId];
+    const finishColumn = columns[destination.droppableId];
+    if (startColumn === finishColumn) {
+      const newTaskIds = Array.from(startColumn.tasksIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...startColumn,
+        tasksIds: newTaskIds,
+      };
+
+      console.log(newColumn, '444');
+      // const newState = {
+      //   ...this.state,
+      //   columns: {
+      //     ...this.state.columns,
+      //     [newColumn.id]: newColumn,
+      //   },
+      // };
+
+      setColumns({
+        ...columns,
+        [newColumn.id]: newColumn,
+      });
+
+      // this.setState(newState);
+
+      // save to localStorage after drop
+      const tasksIdArr = newColumn.tasksIds;
+
+      const localTasksArr = JSON.parse(localStorage.getItem('localTasks'));
+
+      const newTasks = [];
+      tasksIdArr.forEach(el => {
+        const oneTask = localTasksArr.find(t => t.id === el);
+        newTasks.push(oneTask);
+      });
+      localStorage.setItem('localTasks', JSON.stringify(newTasks));
+      updateAllTasksToRedux(newTasks);
+      return;
+    }
+
+    // ця частина коду не використовуєть у звязку з відсутністю інших колонок(columns)
+    const startTaskIds = Array.from(startColumn.tasksIds);
+    startTaskIds.splice(source.index, 1);
+    const newStartColumn = {
+      ...startColumn,
+      tasksIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(finishColumn.tasksIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+
+    const newFinishColumn = {
+      ...finishColumn,
+      tasksIds: finishTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStartColumn.id]: newStartColumn,
+        [newFinishColumn.id]: newFinishColumn,
+      },
+    };
+    await this.setState(newState);
+  };
+
   return (
     <div className={css.wrapTasks}>
       <div className={css.tasks}>
         <DragDropContext
-          // onDragEnd={this.onDragEnd}
+          onDragEnd={onDragEnd}
           // onDragStart={this.onDragStart}
           // onDragUpdate={this.onDragUpdate}
           className={css.dragDropContext}
@@ -74,9 +240,9 @@ const Tasks = ({
                   tasksDraw={tasksDraw}
                   column={column}
                   modalAddTasksOpen={modalAddTasksOpen}
-                  // updateCompleted={this.updateCompleted}
-                  // updateTask={this.updateTask}
-                  // deleteTask={this.deleteTask}
+                  updateCompleted={updateCompleted}
+                  updateTask={updateTask}
+                  deleteTask={deleteTask}
                 />
               );
             })}
@@ -86,7 +252,7 @@ const Tasks = ({
       </div>
       <ModalAddTask
         editTask={editTask}
-        handleResetEditTask={() => setEitTask(null)}
+        handleResetEditTask={() => setEditTask(null)}
       />
     </div>
   );
@@ -154,32 +320,32 @@ const Tasks = ({
 //     this.setState({ editTask: null });
 //   };
 
-//   viewTasks = data => {
-//     // eslint-disable-next-line func-names
-//     const filtered = data.filter(function(el) {
-//       return el != null;
-//     });
-//     // TODO filter by sec/m/h/days
-//     // console.log(filtered, 'filtered');
+// viewTasks = data => {
+//   // eslint-disable-next-line func-names
+//   const filtered = data.filter(function(el) {
+//     return el != null;
+//   });
+//   // TODO filter by sec/m/h/days
+//   // console.log(filtered, 'filtered');
 
-//     const arrIds = filtered?.map(el => el.id);
-//     const { columns } = this.state;
-//     const arr = columns['column-1'].tasksIds;
+//   const arrIds = filtered?.map(el => el.id);
+//   const { columns } = this.state;
+//   const arr = columns['column-1'].tasksIds;
 
-//     this.setState({
-//       tasks: data,
-//       columns: {
-//         'column-1': {
-//           id: 'column-1',
-//           title: 'Сьогодні',
-//           tasksIds: arrIds.length === arr.length ? arr : arrIds,
-//         },
+//   this.setState({
+//     tasks: data,
+//     columns: {
+//       'column-1': {
+//         id: 'column-1',
+//         title: 'Сьогодні',
+//         tasksIds: arrIds.length === arr.length ? arr : arrIds,
 //       },
-//       editTask: null,
-//       columnOrder: ['column-1'],
-//     });
-//     // }
-//   };
+//     },
+//     editTask: null,
+//     columnOrder: ['column-1'],
+//   });
+//   // }
+// };
 //   /*
 //    *   crud methods for REDUX
 //    */
@@ -220,98 +386,98 @@ const Tasks = ({
 //    *   beautiful dnd
 //    */
 
-//   onDragUpdate = update => {
-//     const { destination } = update;
-//     const { tasks } = this.state;
-//     const opacity = destination
-//       ? destination.index / Object.keys(tasks).length
-//       : 0;
+// onDragUpdate = update => {
+//   const { destination } = update;
+//   const { tasks } = this.state;
+//   const opacity = destination
+//     ? destination.index / Object.keys(tasks).length
+//     : 0;
 
-//     document.body.style.backgroundColor = `rgba(170,225,250, ${opacity})`;
+//   document.body.style.backgroundColor = `rgba(170,225,250, ${opacity})`;
 
-//     document.body.style.transition = 'background-color 0.9s ease';
-//   };
+//   document.body.style.transition = 'background-color 0.9s ease';
+// };
 
-//   onDragEnd = async result => {
-//     document.body.style.color = 'inherit';
-//     const { updateAllTasksToRedux } = this.props;
-//     const { columns } = this.state;
-//     const { destination, source, draggableId } = result;
-//     if (!destination) {
-//       return;
-//     }
+// onDragEnd = async result => {
+//   document.body.style.color = 'inherit';
+//   const { updateAllTasksToRedux } = this.props;
+//   const { columns } = this.state;
+//   const { destination, source, draggableId } = result;
+//   if (!destination) {
+//     return;
+//   }
 
-//     if (
-//       source.droppableId === destination.droppableId &&
-//       source.index === destination.index
-//     ) {
-//       return;
-//     }
+//   if (
+//     source.droppableId === destination.droppableId &&
+//     source.index === destination.index
+//   ) {
+//     return;
+//   }
 
-//     const startColumn = columns[source.droppableId];
-//     const finishColumn = columns[destination.droppableId];
-//     if (startColumn === finishColumn) {
-//       const newTaskIds = Array.from(startColumn.tasksIds);
+//   const startColumn = columns[source.droppableId];
+//   const finishColumn = columns[destination.droppableId];
+//   if (startColumn === finishColumn) {
+//     const newTaskIds = Array.from(startColumn.tasksIds);
 
-//       newTaskIds.splice(source.index, 1);
-//       newTaskIds.splice(destination.index, 0, draggableId);
+//     newTaskIds.splice(source.index, 1);
+//     newTaskIds.splice(destination.index, 0, draggableId);
 
-//       const newColumn = {
-//         ...startColumn,
-//         tasksIds: newTaskIds,
-//       };
-
-//       const newState = {
-//         ...this.state,
-//         columns: {
-//           ...this.state.columns,
-//           [newColumn.id]: newColumn,
-//         },
-//       };
-
-//       this.setState(newState);
-
-//       // save to localStorage after drop
-//       const tasksIdArr = newColumn.tasksIds;
-
-//       const localTasksArr = JSON.parse(localStorage.getItem('localTasks'));
-
-//       const newTasks = [];
-//       tasksIdArr.forEach(el => {
-//         const oneTask = localTasksArr.find(t => t.id === el);
-//         newTasks.push(oneTask);
-//       });
-//       localStorage.setItem('localTasks', JSON.stringify(newTasks));
-//       updateAllTasksToRedux(newTasks);
-//       return;
-//     }
-
-//     // ця частина коду не використовуєть у звязку з відсутністю інших колонок(columns)
-//     const startTaskIds = Array.from(startColumn.tasksIds);
-//     startTaskIds.splice(source.index, 1);
-//     const newStartColumn = {
+//     const newColumn = {
 //       ...startColumn,
-//       tasksIds: startTaskIds,
-//     };
-
-//     const finishTaskIds = Array.from(finishColumn.tasksIds);
-//     finishTaskIds.splice(destination.index, 0, draggableId);
-
-//     const newFinishColumn = {
-//       ...finishColumn,
-//       tasksIds: finishTaskIds,
+//       tasksIds: newTaskIds,
 //     };
 
 //     const newState = {
 //       ...this.state,
 //       columns: {
 //         ...this.state.columns,
-//         [newStartColumn.id]: newStartColumn,
-//         [newFinishColumn.id]: newFinishColumn,
+//         [newColumn.id]: newColumn,
 //       },
 //     };
-//     await this.setState(newState);
+
+//     this.setState(newState);
+
+//     // save to localStorage after drop
+//     const tasksIdArr = newColumn.tasksIds;
+
+//     const localTasksArr = JSON.parse(localStorage.getItem('localTasks'));
+
+//     const newTasks = [];
+//     tasksIdArr.forEach(el => {
+//       const oneTask = localTasksArr.find(t => t.id === el);
+//       newTasks.push(oneTask);
+//     });
+//     localStorage.setItem('localTasks', JSON.stringify(newTasks));
+//     updateAllTasksToRedux(newTasks);
+//     return;
+//   }
+
+//   // ця частина коду не використовуєть у звязку з відсутністю інших колонок(columns)
+//   const startTaskIds = Array.from(startColumn.tasksIds);
+//   startTaskIds.splice(source.index, 1);
+//   const newStartColumn = {
+//     ...startColumn,
+//     tasksIds: startTaskIds,
 //   };
+
+//   const finishTaskIds = Array.from(finishColumn.tasksIds);
+//   finishTaskIds.splice(destination.index, 0, draggableId);
+
+//   const newFinishColumn = {
+//     ...finishColumn,
+//     tasksIds: finishTaskIds,
+//   };
+
+//   const newState = {
+//     ...this.state,
+//     columns: {
+//       ...this.state.columns,
+//       [newStartColumn.id]: newStartColumn,
+//       [newFinishColumn.id]: newFinishColumn,
+//     },
+//   };
+//   await this.setState(newState);
+// };
 
 //   render() {
 //     const { modalAddTasksOpen } = this.props;
