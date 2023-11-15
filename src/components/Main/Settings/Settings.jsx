8999e64751df95c716015e456faa9d9412c057f0
kalysh.maxym <503292@ukr.net>
2023-11-15 13:21:54 +0200

    Settings - refact component / add AnimationToogle component

--- src/components/Main/Settings/Settings.jsx
+++ src/components/Main/Settings/Settings.jsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
-import Toogle from '../../commons/Toogle/Toogle';
+// TODO Not use now / in progress when fix animation
+// import AnimationToogle from '../../commons/-AnimationToogle/AnimationToogle';
 
 import css from './Settings.module.scss';
 
-const Settings = ({ isChecked }) => {
-  const isAnimation = useSelector(state => state.global.isAnimationCheck);
-  const dispatch = useDispatch();
-
-  const handleOpen = () => {
-    dispatch({ type: 'IS_ANIMATION_CHECK' });
-  };
-  return (
-    <div className={css.settings}>
-      <div title={isAnimation ? 'is ON' : 'is OFF'} className={css.animation}>
-        <Toogle isChecked={isAnimation} setAnimationCheched={handleOpen} />
-        {isAnimation ? (
-          <span>Анімація включена</span>
-        ) : (
-          <span>Анімація виключена</span>
-        )}
-      </div>
-      {/* Settings in progress */}
-    </div>
-  );
+const Settings = () => {
+  return <div className={css.settings}></div>;
 };
 
 export default Settings;
