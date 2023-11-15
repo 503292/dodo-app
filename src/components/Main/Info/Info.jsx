6a9c1940abe404af212e447aa508fa39f5da147a
kalysh.maxym <503292@ukr.net>
2023-11-15 13:47:48 +0200

    Setting & Info - refact

--- src/components/Main/Info/Info.jsx
+++ src/components/Main/Info/Info.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import MyContacts from '../../commons/MyContacts/MyContacts';
-import QuickAccessGuide from '../../commons/QuickAccessGuide/QuickAccessGuide';
 
 import css from './Info.module.scss';
 
 const Info = () => (
   <div className={css.container}>
-    <QuickAccessGuide />
     <MyContacts />
   </div>
 );
--- src/components/Main/Settings/Settings.jsx
+++ src/components/Main/Settings/Settings.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 // TODO Not use now / in progress when fix animation
 // import AnimationToogle from '../../commons/-AnimationToogle/AnimationToogle';
+import QuickAccessGuide from '../../commons/QuickAccessGuide/QuickAccessGuide';
 
 import css from './Settings.module.scss';
 
 const Settings = () => {
-  return <div className={css.settings}></div>;
+  return (
+    <div className={css.settings}>
+      <QuickAccessGuide />
+    </div>
+  );
 };
 
 export default Settings;
