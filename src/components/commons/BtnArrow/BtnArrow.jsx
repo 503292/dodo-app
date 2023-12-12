ceaf0ef20487016b2b5f9c908c2142def34f219e
kalysh.maxym <503292@ukr.net>
2023-12-12 14:02:02 +0200

    BtnArrow - refact view

--- src/components/commons/BtnArrow/BtnArrow.jsx
+++ src/components/commons/BtnArrow/BtnArrow.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import { ReactComponent as Left } from '../../../assets/image/arrow-l.svg';
 import { ReactComponent as Right } from '../../../assets/image/arrow-r.svg';
+import css from './BtnArrow.module.scss';
 
 const BtnArrow = ({ name = 'Left', handleClick }) => {
   return (
-    <button onClick={handleClick}>
+    <button className={css.btn} onClick={handleClick}>
       {name === 'Left' ? <Left /> : <Right />}
-      {name === 'Left' ? 'Left' : 'Right'}
     </button>
   );
 };
