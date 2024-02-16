a99ef5d042d2904e1b9f27b22cc0dc620ad7a2df
kalysh.maxym <503292@ukr.net>
2024-02-16 17:35:04 +0200

    some fix

--- src/components/commons/ToogleReminder/ToogleReminder.jsx
+++ src/components/commons/ToogleReminder/ToogleReminder.jsx
@@ -1,30 +1,29 @@
-import React, { useState } from 'react';
+import React from // , { useState }
+'react';
 
-import Toogle from '../Toogle/Toogle';
-import { requestNotificationPermission } from '../../../utils/requestNotificationPermission';
+// import Toogle from '../Toogle/Toogle';
+// import { requestNotificationPermission } from '../../../utils/requestNotificationPermission';
 
-import css from './ToogleReminder.module.scss';
+// import css from './ToogleReminder.module.scss';
 
 const ToogleReminder = () => {
-  const [isPushNotification, setIsPushNotification] = useState(
-    Notification.permission === 'granted',
-  );
+  // const [isPushNotification, setIsPushNotification] = useState(false);
 
-  const handleNotification = async e => {
-    e.preventDefault();
-    const isPermission = await requestNotificationPermission();
-    if (isPermission) return setIsPushNotification(isPermission);
-  };
+  // const handleNotification = async e => {
+  //   e.preventDefault();
+  //   const isPermission = await requestNotificationPermission();
+  //   if (isPermission) return setIsPushNotification(isPermission);
+  // };
   return (
     <>
-      <div title={isPushNotification ? 'is ON' : 'is OFF'} className={css.wrap}>
+      {/* <div title={isPushNotification ? 'is ON' : 'is OFF'} className={css.wrap}>
         <Toogle
           isChecked={isPushNotification}
           handleCheck={e => handleNotification(e)}
         />
         <span>Сповіщення</span>
         {isPushNotification ? <span>включені</span> : <span>виключені</span>}
-      </div>
+      </div> */}
     </>
   );
 };
