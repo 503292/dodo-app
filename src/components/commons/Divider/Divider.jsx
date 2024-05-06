d5ac8312b33d48e806908ed7155525bee38d730c
kalysh.maxym <503292@ukr.net>
2024-05-06 22:40:45 +0300

    MathGame #4 - refact styles

---
+++ src/components/commons/Divider/Divider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import css from './Divider.module.scss';
+
+const Divider = () => {
+  return (
+    <>
+      <div className={css.divider} />
+    </>
+  );
+};
+
+export default Divider;
--- src/components/commons/MathGame/MathGame.jsx
+++ src/components/commons/MathGame/MathGame.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useMemo } from 'react';
-// import second from '../';
+import React, { useState, useMemo, Fragment } from 'react';
+import Divider from '../Divider/Divider';
 import css from './MathGame.module.scss';
 
 function randomIntFromInterval(min, max) {
@@ -11,17 +11,18 @@ const MathGame = () => {
   const [count, setCount] = useState(0);
   const [answer, setAnswer] = useState(null);
   const [inputValue, setInputValue] = useState('');
+  const [answersArr, setAnswersArr] = useState([1, 2, 3, 4]);
 
   const mathTask = useMemo(() => {
-    const firstNum = randomIntFromInterval(1, 5);
-    const secondNum = randomIntFromInterval(1, 5);
+    const firstNum = randomIntFromInterval(0, 2);
+    const secondNum = randomIntFromInterval(1, 2);
     setAnswer(firstNum + secondNum);
     return `${firstNum} + ${secondNum}`;
   }, [count]);
 
   function handleSubmit(e) {
     e.preventDefault();
-
+    console.log(answer, '===', inputValue);
     validateAnswer();
   }
 
@@ -38,23 +39,55 @@ const MathGame = () => {
   return (
     <form className={css.wrapGame} onSubmit={handleSubmit}>
       <h2>Math Game</h2>
-      <p className={css.mathTask}>{mathTask}</p>
-      <input
-        type="number"
-        inputMode="numeric"
-        pattern="\d*"
-        value={inputValue}
-        onChange={e => setInputValue(+e.target.value)}
-      />
-      <div className={css.wrapBtns}>
-        <button type="button" onClick={() => setInputValue('')}>
-          Скинути
-        </button>
-        <button type="submit">Відповісти</button>
+      <div className={css.mathTask}>
+        <p>
+          {mathTask} <br /> =
+        </p>
+        <input
+          type="number"
+          inputMode="numeric"
+          pattern="\d*"
+          placeholder="?"
+          value={inputValue}
+          onChange={e => setInputValue(+e.target.value)}
+        />
       </div>
-      <div className={css.counter}> Counter = {count}</div>
+      <Divider />
+
+      <div className={css.wrapAnswers}>
+        {answersArr.map((el, idx) => (
+          <Fragment key={idx}>
+            <AnswerBlock text={el} handleClick={setInputValue} />
+          </Fragment>
+        ))}
+      </div>
+      <Divider />
+      <div className={css.footer}>
+        <span>Лічильник: {count}</span>
+        <button type="submit" className={`animateBtn`}>
+          Відповісти
+        </button>
+      </div>
     </form>
   );
 };
 
 export default MathGame;
+
+const AnswerBlock = ({ text, handleClick = () => {} }) => {
+  // TODO generator random answer with include TRUE answer
+  return (
+    <button type="button" onClick={() => handleClick(text)}>
+      {text} - // - TODO
+    </button>
+  );
+};
+
+{
+  /* <div className={css.wrapBtns}>
+        <button type="button" onClick={() => setInputValue('')}>
+          Скинути
+        </button>
+        <button type="submit">Відповісти</button>
+      </div> */
+}
