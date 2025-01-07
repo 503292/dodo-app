f91ca42aa508bbcd0e5025536fb0dab51e5352c4
kalysh.maxym <503292@ukr.net>
2025-01-07 16:07:42 +0200

    MathInfoPage - add template, add images to assets, connect route

---
+++ src/components/Main/MathInfo/MathInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MathInfo = () => {
+  return <>TODO add images for page MathInfo</>;
+};
+
+export default MathInfo;
---
+++ src/pages/MathInfoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MathInfo from '../components/Main/MathInfo/MathInfo';
+
+const MathInfoPage = () => {
+  return (
+    <>
+      <MathInfo />
+    </>
+  );
+};
+
+export default MathInfoPage;
