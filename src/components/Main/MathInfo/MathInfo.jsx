eb17274c4592f297839f4548530e631ed7842139
kalysh.maxym <503292@ukr.net>
2025-01-07 23:35:04 +0200

    MathInfo - add style, connect images, add description #2

--- src/components/Main/MathInfo/MathInfo.jsx
+++ src/components/Main/MathInfo/MathInfo.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
+import start from '../../../assets/image/mathInfo/1-start.png';
+import game from '../../../assets/image/mathInfo/2-game.png';
+import profile from '../../../assets/image/mathInfo/3-profile.png';
+
+import css from './MathInfo.module.scss';
+const about = {
+  start: 'Початок гри самий захоплюючий.',
+  game: 'Функціонал і налаштування',
+  profile: 'Пофайл і налаштування',
+};
+
 const MathInfo = () => {
-  return <>TODO add images for page MathInfo</>;
+  return (
+    <div className={css.wrap}>
+      <InfoImage src={start} description={about.start} />
+      <InfoImage src={game} description={about.game} />
+      <InfoImage src={profile} description={about.profile} />
+    </div>
+  );
 };
 
 export default MathInfo;
+
+const InfoImage = ({ src = '#', description = '' }) => {
+  return (
+    <>
+      <div>{description}</div>
+      <div className={css.wrapImg}>
+        <img className={css.img} src={src} alt={src} />
+      </div>
+    </>
+  );
+};
