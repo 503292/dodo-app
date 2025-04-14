5aca6b0ade3657dee273b42c63c73ba82dd74688
kalysh.maxym <503292@ukr.net>
2025-04-14 10:01:26 +0300

    change info icon to donate / some fixes / delete not use icon

--- src/components/commons/DropdownProfile/DropdownProfile.jsx
+++ src/components/commons/DropdownProfile/DropdownProfile.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import { ReactComponent as Info } from '../../../assets/image/nav/info.svg';
+// import { ReactComponent as Info } from '../../../assets/image/nav/info.svg';
+import { ReactComponent as Donate } from '../../../assets/image/nav/donate.svg';
 import { ReactComponent as Settings } from '../../../assets/image/nav/setting.svg';
-import { ReactComponent as Profile } from '../../../assets/image/nav/profile.svg';
+// import { ReactComponent as Profile } from '../../../assets/image/nav/profile.svg';
 import { ReactComponent as Notes } from '../../../assets/image/nav/notes.svg';
 import { ReactComponent as Game } from '../../../assets/image/nav/game.svg';
 
@@ -21,35 +22,40 @@ const DropdownProfile = ({ isActive, refProfile }) => {
         <nav ref={refProfile} className={css.wrapDropdownSettings}>
           <ul className={css.listPages}>
             <li className={css.page}>
-              <NavLink exact activeStyle={ativeStyle} to="/info">
-                <Info />
+              <NavLink exact activeStyle={ativeStyle} to="/lang_game">
+                EN
               </NavLink>
             </li>
-            <li className={css.page}>
-              <NavLink exact activeStyle={ativeStyle} to="/settings">
-                <Settings />
-              </NavLink>
-            </li>
-            <li className={css.page}>
-              <NavLink exact activeStyle={ativeStyle} to="/profile">
-                <Profile />
-              </NavLink>
-            </li>
-            <li className={css.page}>
-              <NavLink exact activeStyle={ativeStyle} to="/menu">
-                <Notes />
-              </NavLink>
-            </li>
+
             <li className={css.page}>
               <NavLink exact activeStyle={ativeStyle} to="/math_game">
                 <Game />
               </NavLink>
             </li>
+
             <li className={css.page}>
-              <NavLink exact activeStyle={ativeStyle} to="/lang_game">
-                EN
+              <NavLink exact activeStyle={ativeStyle} to="/menu">
+                <Notes />
               </NavLink>
             </li>
+
+            <li className={css.page}>
+              <NavLink exact activeStyle={ativeStyle} to="/info">
+                <Donate />
+              </NavLink>
+            </li>
+
+            <li className={css.page}>
+              <NavLink exact activeStyle={ativeStyle} to="/settings">
+                <Settings />
+              </NavLink>
+            </li>
+
+            {/* <li className={css.page}>
+              <NavLink exact activeStyle={ativeStyle} to="/profile">
+                <Profile />
+              </NavLink>
+            </li> */}
           </ul>
         </nav>
       ) : null}
