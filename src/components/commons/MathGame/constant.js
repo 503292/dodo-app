2b151f51c4dce3cec0381968ae4644185c237292
kalysh.maxym <503292@ukr.net>
2025-12-31 19:37:56 +0200

    MathGame - refact styles, constants

--- src/components/commons/MathGame/constant.js
+++ src/components/commons/MathGame/constant.js
@@ -1,7 +1,11 @@
-export const MIN = [0, -10, -20, -50, -100];
-export const MAX = [0, 10, 20, 50, 100];
-// TODO  =>  in progress '%',  '√²'
-export const OPERATORS = ['+', '-', '*', 'x²', 'x÷2'];
+// export const MIN = [0, -10, -20, -50, -100];
+// export const MAX = [0, 10, 20, 50, 100];
+// // TODO  =>  in progress '%',  '√²'
+// export const OPERATORS = ['+', '-', '*', 'x²', 'x÷2'];
+
+export const MIN = [-100, -50, -20, -10, 0];
+export const MAX = [100, 50, 20, 10, 0];
+export const OPERATORS = [, 'x²', 'x÷2', '×', '-', '+']; // x!, x % y
 
 // export const MIN = [-10, 0];
 // export const MAX = [0, 10];
