cf7db0d3a4ddaea677c69bbd808d0aea34aba3d5
kalysh.maxym <503292@ukr.net>
2025-12-31 20:48:18 +0200

    MathGame - refact footer | refact Counter styles

--- src/components/commons/MathGame/Counter.jsx
+++ src/components/commons/MathGame/Counter.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import CircleButton from './CircleButton';
+import { ReactComponent as Lamp } from '../../../assets/image/lamp.svg';
+
+import css from './MathGame.module.scss';
 
 const Counter = ({ count }) => {
-  return <>Counter: {count}</>;
+  return (
+    <div className={css.wrapLamp}>
+      <CircleButton>
+        <Lamp />
+      </CircleButton>
+      : {count}
+    </div>
+  );
 };
 
 export default Counter;
--- src/components/commons/MathGame/Footer.jsx
+++ src/components/commons/MathGame/Footer.jsx
@@ -1,12 +1,11 @@
 import React, { useMemo } from 'react';
 import css from './MathGame.module.scss';
 
-const Footer = ({ count, userAnswer }) => {
+const Footer = ({ userAnswer }) => {
   const isDisable = useMemo(() => userAnswer === '', [userAnswer]);
 
   return (
     <div className={css.footer}>
-      <span>Лічильник: {count}</span>
       <button
         type="submit"
         style={isDisable ? { background: 'transparent' } : {}}
